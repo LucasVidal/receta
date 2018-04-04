89cc32ac53e8349ed6332f9e6e30367b09d74e0d
Lucas Vidal <lvidal@twilio.com>
2018-04-04 12:54:45 -0500

    change db to PG

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20180316143033) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "receta", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
